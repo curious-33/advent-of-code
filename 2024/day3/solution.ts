a895e1e828d734d4c581f9dc263f68a4c4f0dfbd
Curious <curious.startpro@gmail.com>
2024-12-04 10:59:09 +0500

    chore: day3#part

--- 2024/day3/solution.ts
+++ 2024/day3/solution.ts
@@ -6,19 +6,35 @@ function mul(x: number, y: number) {
 	return x * y
 }
 
-function findUncorruptedMulFunctions(data: string) {
+// function findUncorruptedMulFunctions(data: string) {
+// 	let total = 0
+// 	const regex = /mul\(\d{1,3},\d{1,3}\)/g
+// 	const mulFns = data.match(regex) || []
+
+// 	mulFns
+// 		.map((el) => el.split(/mul\(|\)/))
+// 		.map((el) => el[1].split(','))
+// 		.forEach((el) => {
+// 			total += mul(+el[0], +el[1])
+// 		})
+
+// 	return total
+// }
+
+// console.log(findUncorruptedMulFunctions(getInput()))
+
+function findUncorruptedEnabledMulFunctions(data: string) {
 	let total = 0
-	const regex = /mul\(\d{1,3},\d{1,3}\)/g
+	const regex = /don\'t\(\)|do\(\)|mul\(\d{1,3},\d{1,3}\)/g
 	const mulFns = data.match(regex) || []
 
-	mulFns
-		.map((el) => el.split(/mul\(|\)/))
-		.map((el) => el[1].split(','))
-		.forEach((el) => {
-			total += mul(+el[0], +el[1])
-		})
+	// .map((el) => el.split(/mul\(|\)/))
+	// .map((el) => el[1].split(','))
+	// .forEach((el) => {
+	// 	total += mul(+el[0], +el[1])
+	// })
 
-	return total
+	return mulFns
 }
 
-console.log(findUncorruptedMulFunctions(getInput()))
+console.log(findUncorruptedEnabledMulFunctions(getInput()))
